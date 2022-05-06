9d1292fd3098e7211f0b8585d495fa88e62fd772
tomohiro1024 <gongyezhihong732@gmail.com>
2022-05-06 18:21:30 +0900

    Mapの表示

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -8,11 +9,25 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  late GoogleMapController _controller;
+  // Google Mapの初期表示
+  static const CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(35.71027175858155, 139.8107862279798),
+    zoom: 16,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Google Map'),
+    return Scaffold(
+      body: GoogleMap(
+        // 衛生写真かノーマルを表示
+        mapType: MapType.hybrid,
+        //　Mapの初期位置
+        initialCameraPosition: _initialPosition,
+        // Mapが作成されたタイミングの処理
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+        },
       ),
     );
   }
