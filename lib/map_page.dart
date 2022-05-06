4d0e4e5cd8167875f900e6a80ec4fd9e099d95fc
tomohiro1024 <gongyezhihong732@gmail.com>
2022-05-06 18:39:39 +0900

    ピンの表示

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -13,13 +13,27 @@ class _MapPageState extends State<MapPage> {
   // Google Mapの初期表示
   static const CameraPosition _initialPosition = CameraPosition(
     target: LatLng(35.71027175858155, 139.8107862279798),
-    zoom: 16,
+    zoom: 17,
   );
+
+  // ピンの表示
+  final Set<Marker> _markers = {
+    const Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(35.71027175858155, 139.8107862279798),
+      infoWindow: InfoWindow(title: '東京スカイツリー', snippet: '634M'),
+    ),
+    const Marker(
+      markerId: MarkerId('2'),
+      position: LatLng(35.71055052892032, 139.8091661738491),
+    ),
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
+        markers: _markers,
         // 衛生写真かノーマルを表示
         mapType: MapType.hybrid,
         //　Mapの初期位置
